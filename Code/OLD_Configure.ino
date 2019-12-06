d3cb96c518b0e90a2d52e9013fb03c1b6a4661c9
Dmytrocode <55360457+Dmytrocode@users.noreply.github.com>
2019-12-06 13:07:20 -0600

    Update and rename Configure.ino to OLD_Configure.ino

---
+++ Code/OLD_Configure.ino
@@ -0,0 +1,27 @@
+/*
+Created by Shubham Shinganapure
+modified by Dmytro Kavetskyy 04 December 2019
+
+===== This is a very simple program that will allow the two bluetooth devices to interact and connect with each other =======
+*/
+
+#include <SoftwareSerial.h> 
+SoftwareSerial BTSerial(10, 11); // RX | TX
+void setup()
+{
+Serial.begin(9600);
+Serial.println("Enter AT commands:");
+BTSerial.begin(38400); // HC-05 default speed in AT command more
+}
+void loop()
+{
+
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
+
